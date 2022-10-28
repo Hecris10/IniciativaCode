5dd7097d8f123732900ee8e3355d2f0784d1586b
Helamã Ewerton <helamaewerton@gmail.com>
2022-10-27 21:40:21 -0300

    Adicionando aula de Classes

---
+++ Code Now!/Classes/Aula 01/EtudoClasse/EtudoClasse/Pessoa.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtudoClasse
+{
+    public class Pessoa
+    {
+        //Atributos
+        private string Nome;
+        private int Idade;
+        private char Sexo;
+        private string Endereco;
+        //Construtor
+        public Pessoa(string Nome, int Idade, char Sexo)
+        {
+           this.Nome = Nome;
+           this.Idade = Idade;
+           this.Sexo = Sexo;
+        }
+        //Metodos
+        public string getNome()
+        {
+            return this.Nome;
+        }
+
+        public int getIdade()
+        {
+            return this.Idade;
+        }
+
+        public char getSexo()
+        {
+            return this.Sexo;
+        }
+
+        public string getEndereco()
+        {
+            if (this.Endereco == "") return null;
+
+            return this.Endereco;
+        }
+
+        public void setNome(String novoNome)
+        {
+            this.Nome = novoNome;
+        }
+        public void setIdade(int novaIdade)
+        {
+            this.Idade = novaIdade;
+        }
+
+        public void setSexo(char novoSexo)
+        {
+            this.setIdade(novoSexo);
+        }
+
+        public void setEndereco(string novoEndereco)
+        {
+            this.Endereco = novoEndereco;
+        }
+    }
+}
---
+++ Code Now!/Classes/Aula 01/EtudoClasse/EtudoClasse/Program.cs
@@ -0,0 +1,31 @@
+﻿using EtudoClasse;
+
+List<Pessoa> listaDePessoas = new List<Pessoa>();
+
+int numeroPessoas = int.Parse(Console.ReadLine());
+
+for(int i=0; i < numeroPessoas; i++)
+{
+    Console.WriteLine("Insira o Nome da Pessoa: ");
+    string nome = Console.ReadLine();
+    Console.WriteLine("Insira a Idade da Pessoa: ");
+    int idade = int.Parse(Console.ReadLine());
+    Console.WriteLine("Insira o Sexo da Pessoa(M ou F): ");
+    char sexo = char.Parse(Console.ReadLine());
+
+    Pessoa novaPessoa = new Pessoa(nome, idade, sexo);
+
+    Console.WriteLine("Nova Pessoa Cadastrada com sucesso!!!\n");
+
+    listaDePessoas.Add(novaPessoa);
+}
+
+
+for (int i = 0; i < listaDePessoas.Count(); i++)
+{
+    Pessoa usuario = listaDePessoas[i];
+    Console.WriteLine($"Usuario {i + 1}: - Nome:{usuario.getNome()} Idade: {usuario.getIdade()} " +
+        $"Sexo: {usuario.getSexo()}\n");
+}
+
+Console.WriteLine("Acabou o programa");
